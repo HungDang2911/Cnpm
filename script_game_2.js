d7a39002b7927d1bee5e7edd7247a39764a9905c
18020042 <57447835+18020042@users.noreply.github.com>
2020-04-21 20:25:31 +0700

    #5 Xong giao diện màn chơi
    Chưa có các chức năng để chơi được

--- script_game_2.js
+++ script_game_2.js
@@ -11,3 +11,15 @@ function allowDrop(ev) {
     var data = ev.dataTransfer.getData("text");
     ev.target.appendChild(document.getElementById(data));
   }
+
+  function createPrice(){
+    var rand = Math.floor(Math.random() * 90 + 10); 
+    document.getElementById("price").innerHTML = "Giá tiền: <br>"  + rand + "$";
+  }
+
+  function buy(callback){
+    document.getElementById("buy-btn").addEventListener('click',callback);
+  }
+
+
+  createPrice();
