35a885b7a4109568d1d11e9f661e6fb18a88c452
18020042 <57447835+18020042@users.noreply.github.com>
2020-04-22 18:54:46 +0700

    #5 Thêm hàm
    Thêm hàm xử lí khi đúng hoặc sai giá, thêm hàm phụ reset màn chơi

--- script_game_2.js
+++ script_game_2.js
@@ -21,8 +21,55 @@ function allowDrop(ev) {
     document.getElementById("buy-btn").addEventListener('click',callback);
   }
 
-  function caculate(){
-    
+  function rightPrice(){
+    var elem = document.getElementById("tray");   
+    elem.innerHTML = "<br>Đã nhận tiền !";
+    elem.style.opacity = '0.7';
+    elem.style.background = 'white';
+    var pos = 0;
+    var id = setInterval(frame, 3);
+    function frame() {
+      if (pos == 300) {
+        clearInterval(id);
+        resetScreen();
+        createPrice();
+      } else {
+        pos++; 
+        elem.style.top = pos + "px"; 
+      }
+    }
   }
 
+  
+  function wrongPrice(){
+    var elem = document.getElementById("tray");   
+
+    elem.innerHTML = "<br>Không đúng số tiền mất rồi !";
+    elem.style.background = 'red';
+    elem.style.opacity = '0.8';
+    
+    var pos = 0;
+    var id = setInterval(frame, 10);
+    function frame() {
+      if (pos == 150) {
+        clearInterval(id);
+        resetScreen();
+        createPrice();
+      } else {
+        pos++; 
+        elem.style.top = pos + "px"; 
+      }
+    }
+  }
+
+  function resetScreen(){
+    var container = document.getElementById("play-scr");
+    container.innerHTML= html;
+}                
+    var html;
+    window.onload = function(){
+	  html = document.getElementById('play-scr').innerHTML;
+};     
+
   createPrice();
+  buy(wrongPrice);
